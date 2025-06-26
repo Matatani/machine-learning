04d574fbc38acceebecdacaf7d2413261214d693
maevlava <maevlava@gmail.com>
2025-06-26 18:10:09 +0700

    Python as grpc server

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 import grpc
 import predictor_pb2_grpc
 import predictor_pb2
+import server
+
 
 def main():
-    channel = grpc.insecure_channel('localhost:50051')
-    stub = predictor_pb2_grpc.PredictorStub(channel)
-
-    greeting = predictor_pb2.Greeting(greeting='Hello, world!', name='World')
-    request = predictor_pb2.HelloRequest(greeting=greeting)
-    setattr(request, "from", "Python Server")
-
-    response = stub.Hello(request)
-    print("Go server responded:", response.greeting)
+    server.serve()
 
 if __name__ == "__main__":
     main()
---
+++ server.py
@@ -0,0 +1,19 @@
+from concurrent import futures
+
+import grpc
+import predictor_pb2
+import predictor_pb2_grpc
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    predictor_pb2_grpc.add_PredictorServicer_to_server(PredictorServicer(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    server.wait_for_termination()
+
+
+class PredictorServicer(predictor_pb2_grpc.PredictorServicer):
+    def Hello(self, request, context):
+        return predictor_pb2.HelloResponse(greeting=f"Hello, {request.greeting.name}!")
+
