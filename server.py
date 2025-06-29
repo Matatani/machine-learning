a8ac93b7108e1a6e73bc36e96fab20e86e20281b
maevlava <maevlava@gmail.com>
2025-06-30 03:55:39 +0700

    Serve predictor
    update requirements

--- server.py
+++ server.py
@@ -1,8 +1,12 @@
 from concurrent import futures
 
 import grpc
+
+from predictor.predictor import PaddyPredictor
 import predictor_pb2
 import predictor_pb2_grpc
+import boto3
+import os
 
 
 def serve():
@@ -15,8 +19,38 @@ def serve():
 
 
 class PredictorServicer(predictor_pb2_grpc.PredictorServicer):
+    def __init__(self):
+        self.predictor = PaddyPredictor()
+        self.s3 = boto3.client(
+            's3',
+            endpoint_url=os.getenv('S3_ENDPOINT'),
+            aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+            aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+            region_name=os.getenv('AWS_REGION')
+        )
+
+
     def Hello(self, request, context):
         return predictor_pb2.HelloResponse(greeting=f"Hello, {request.greeting.name}!")
 
     def PredictImage(self, request, context):
-        return predictor_pb2.PredictImageResponse(className=f"Disease: Bacterial Blight")
+        print("PredictImage called with request:", request)
+        local_path = f'/tmp/{request.key}'
+        try:
+            self.s3.download_file(request.bucket, request.key, local_path)
+            print("Download successful:", local_path)
+        except Exception as e:
+            print("Error downloading from S3:", e)
+            context.set_details(str(e))
+            context.set_code(grpc.StatusCode.INTERNAL)
+            return predictor_pb2.PredictImageResponse(className="Error")
+
+        try:
+            predicted_label = self.predictor.predict_image(local_path)
+            print("Prediction successful:", predicted_label)
+            return predictor_pb2.PredictImageResponse(className=f'The disease is {predicted_label}')
+        except Exception as e:
+            print("Error during prediction:", e)
+            context.set_details(str(e))
+            context.set_code(grpc.StatusCode.INTERNAL)
+            return predictor_pb2.PredictImageResponse(className="Error")
